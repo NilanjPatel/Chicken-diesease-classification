10b5d7b5ebd42ed51523fd150ec08f40c8d1dc06
Nilanj <nilanjpatel@yahoo.in>
2023-08-20 23:34:58 -0400

    pipeline added

--- main.py
+++ main.py
@@ -1,4 +1,14 @@
 from cnnClassifire import logger
+from cnnClassifire.pipline.stage_01_data_ingestion import DataIngestionTrainingPipeling
 
-logger.info("Welcome to my Cnn project ")
+STAGE_NAME = "Data Ingestion stage"
 
+
+try:
+    logger.info(f">>>>> stage {STAGE_NAME} started <<<<")
+    stage_1_obj = DataIngestionTrainingPipeling()
+    stage_1_obj.main()
+    logger.info(f">>>>> stage {STAGE_NAME} completed <<<<<\n\nx====================x")
+except Exception as e:
+    logger.exception(e)
+    raise e
---
+++ src/cnnClassifire/pipline/stage_01_data_ingestion.py
@@ -0,0 +1,37 @@
+from cnnClassifire.components.data_ingestion import DataIngestion
+from cnnClassifire.config.configuration import ConfigurationManager
+from cnnClassifire import logger
+
+try:
+    config = ConfigurationManager()
+    data_ingestion_config = config.get_data_ingestion_config()
+    data_ingestion = DataIngestion(config=data_ingestion_config)
+    data_ingestion.download_file()
+    data_ingestion.extract_zip_file()
+except Exception as e:
+    raise e
+
+STAGE_NAME = "Data Ingestion stage"
+
+class DataIngestionTrainingPipeling:
+    def __init__(self):
+        pass
+
+    def main(self):
+        config = ConfigurationManager()
+        data_ingestion_config = config.get_data_ingestion_config()
+        data_ingestion = DataIngestion(config=data_ingestion_config)
+        data_ingestion.download_file()
+        data_ingestion.extract_zip_file()
+
+
+if __name__ == '__main__':
+    try:
+        logger.info(f">>>>> stage {STAGE_NAME} started <<<<")
+        stage_1_obj = DataIngestionTrainingPipeling()
+        stage_1_obj.main()
+        logger.info(f">>>>> stage {STAGE_NAME} completed <<<<<\n\nx====================x")
+    except Exception as e:
+        logger.exception(e)
+        raise e
+
